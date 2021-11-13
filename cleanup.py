38b5d523f8d6086e2c67762df05f17f9097d6951
Tiago Barradas <64672906+barrafas@users.noreply.github.com>
2021-11-13 19:00:54 -0300

    implementing new database

--- cleanup.py
+++ cleanup.py
@@ -1,20 +1,31 @@
 import os
-import shutil
 
-with open('lfw-names.txt') as f:
+# script that cleans the original downloaded images taken from the https://conradsanderson.id.au/lfwcrop/ website
+
+img_count = 8 # exact number of images you want from each person from the dataset
+
+with open('lfw-names.txt') as f: # reads the names.txt file, which has the name and number of images of every person in the dataset
     lines = f.readlines()
 
-names = [line.split()[0] for line in lines if line.split()[1] == "5"]
-
-dirs = os.listdir('lfw-deepfunneled/')
-destination = 'images'
-for dir in dirs:
-    if dir not in names:
-        for file in os.listdir(os.path.join('lfw-deepfunneled', dir)):
-            os.remove(os.path.join('lfw-deepfunneled', dir, file))
-    else:
-        for file in os.listdir(os.path.join('lfw-deepfunneled', dir)):
-            shutil.move(os.path.join('lfw-deepfunneled', dir, file), os.path.join(destination, file))
+names = [line.split()[0] for line in lines if int(line.split()[1]) >= img_count] # filters names based on the number of images they have
 
 
+files = os.listdir('faces/') # name of the directory which has the person-specific subdirectories
 
+for file in files:
+    if file[:file.find('0')-1] not in names: # treats the file name string to match with the names on the txt file
+        print(file)
+        os.remove(os.path.join('faces', file)) # removes files from names that don't have 8 or more images
+
+
+# loop that counts the number of images of each person, and deletes the excess images if they have more than 8
+for name in names: 
+    count = 0
+    for file in files:
+        if file[:file.find('0')-1] == name:
+            count += 1
+            if count > img_count:
+                print(file)
+                os.remove(os.path.join('faces', file)) 
+        
+print(names)
